5a1e3991d1561e1b4d2c53a0a250bb9e36727ddc
pclaesen <pieterclaesen@gmail.com>
2024-07-04 12:25:36 +0000

    Single Request function

--- githubstreaming/src/app/page.js
+++ githubstreaming/src/app/page.js
@@ -4,62 +4,119 @@ import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 
 import { Web3SignatureProvider } from "@requestnetwork/web3-signature";
-
-let signer;
+import { RequestNetwork, Types, Utils } from "@requestnetwork/request-client.js";
 
 export default function Home() {
   const [account, setAccount] = useState(null);
-  const [provider, setProvider] = useState(null);
-  const [web3Provider, setWeb3Provider] = useState(null);
 
-  useEffect(() => {
-    if (account) {
-      console.log("Your account:", account);
-    }
-  }, [account]);
+  // useEffect(() => {
+  //   if (account) {
+  //     console.log("Your account:", account);
+  //   }
+  // }, [account]);
 
-  useEffect(() => {
-    if (provider) {
-      console.log("Your provider:", provider);
-    }
-  }, [provider]);
+  // useEffect(() => {
+  //   if (provider) {
+  //     console.log("Your provider:", provider);
+  //   }
+  // }, [provider]);
 
-  useEffect(() => {
-    if (web3Provider) {
-      console.log("Your Request Network web3Provider:", web3Provider);
-    }
-  }, [web3Provider]);
+  // useEffect(() => {
+  //   if (web3SignatureProvider) {
+  //     console.log("Your Request Network web3SignatureProvider:", web3SignatureProvider);
+  //   }
+  // }, [web3SignatureProvider]);
+
+  // useEffect(() => {
+  //   if (requestClient) {
+  //     console.log("Your RequestClient:", requestClient);
+  //   }
+  // }, [requestClient]);
+
+  // useEffect(() => {
+  //   if (requestCreateParameters) {
+  //     console.log("Your requestCreateParameters:", requestCreateParameters);
+  //   }
+  // }, [requestCreateParameters]);
 
   async function connectToMetaMask() {
     if (typeof window.ethereum !== 'undefined') {
-      const newProvider = new ethers.providers.Web3Provider(window.ethereum);
-        setProvider(newProvider);
       try {
-        // Request account access
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
         setAccount(accounts[0]);
-        
-        // Optionally, you can set up a provider with ethers.js
-        
       } catch (error) {
         console.error('User rejected the request');
       }
     } else {
       console.error('MetaMask is not installed');
+      alert("Install Metamask please");
     }
   }
 
-  async function createRequestWeb3Provider() {
-    if (typeof window.ethereum !== 'undefined') {
+  async function handleRequest() {
+    if (typeof window.ethereum !== 'undefined' && account) {
       try {
-        const newWeb3Provider = new Web3SignatureProvider(window.ethereum);
-        setWeb3Provider(newWeb3Provider);
-        
+        // Step 1: Create Request Web3 Provider
+        const web3Provider = new Web3SignatureProvider(window.ethereum);
+
+        // Step 2: Create Request Client
+        const tempRequestClient = new RequestNetwork({
+          nodeConnectionConfig: {
+            baseURL: "https://sepolia.gateway.request.network/",
+          },
+          signatureProvider: web3Provider,
+        });
+
+        // Step 3: Create Request Parameters
+        const payeeIdentity = '0xc573Cc1F309CA15FD120e50bE0166a081f253A48';
+        const payerIdentity = '0x1B39C76b7bbF7F16795F461Ce5298E882B63a7D6';
+        const requestCreateParameters = {
+          requestInfo: {
+            currency: {
+              type: Types.RequestLogic.CURRENCY.ERC20,
+              value: '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238',
+              network: 'sepolia',
+            },
+            expectedAmount: '1000000000000000000',
+            payee: {
+              type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
+              value: payeeIdentity,
+            },
+            payer: {
+              type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
+              value: payerIdentity,
+            },
+            timestamp: Utils.getCurrentTimestampInSecond(),
+          },
+          paymentNetwork: {
+            id: Types.Extension.PAYMENT_NETWORK_ID.ERC20_FEE_PROXY_CONTRACT,
+            parameters: {
+              paymentNetworkName: 'sepolia',
+              paymentAddress: payeeIdentity,
+              feeAddress: '0x0000000000000000000000000000000000000000',
+              feeAmount: '0',
+            },
+          },
+          contentData: {
+            reason: '🍕',
+            dueDate: '2025.06.16',
+          },
+          signer: {
+            type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
+            value: payeeIdentity,
+          },
+        };
+
+        // Step 4: Create New Request with Parameters
+        const request = await tempRequestClient.createRequest(requestCreateParameters);
+        const confirmedRequestData = await request.waitForConfirmation();
+        console.log(confirmedRequestData);
       } catch (error) {
-        console.error('web3provider fetching failed', error);
+        console.error('Request handling failed', error);
       }
     } else {
-      console.log("Cant do web3provider");
+      console.error('No MetaMask connection found, redirecting to connection process...');
+      connectToMetaMask();
     }
   }
 
@@ -72,13 +129,10 @@ export default function Home() {
       </Head>
 
       <main>
-        <button onClick={connectToMetaMask}>Connect</button>
-        <button onClick={createRequestWeb3Provider}>Create Request web3provider</button>
+        <button onClick={connectToMetaMask}>Connect</button><br/>
+        <button onClick={handleRequest}>Handle Request</button><br/>
+        {account && <p>Connected Account: {account}</p>}
       </main>
-
-      <footer>
-        <p>Powered by Next.js and Ethers.js</p>
-      </footer>
     </div>
   );
 }
